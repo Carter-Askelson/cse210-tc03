e271c616878cb530af07220ab8dac9cb4366fd4a
prograhamming <grahamhwalker@outlook.com>
2021-09-22 16:51:39 -0500

    adding to readme, create personal python file, and method to main.py

--- Python-Folder/__main__.py
+++ Python-Folder/__main__.py
@@ -1,9 +1,11 @@
 from askelson import display_greeting
+from gwalker import greeting_from_graham
 
 def main():
     print("Welcome to our collaborative program.")
     print()
     display_greeting()
+    greeting_from_graham()
 
 if __name__ == "__main__":
     main()
